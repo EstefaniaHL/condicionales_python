10112fdd9e2cd08e6d945c15cfe202e88b8122a8
EstefaniaHL <105552739+EstefaniaHL@users.noreply.github.com>
2022-06-08 20:04:41 -0300

    Finalizado
    Completo

--- ejercicios_practica/ejercicio_2.py
+++ ejercicios_practica/ejercicio_2.py
@@ -10,7 +10,7 @@
 # Ejemplos variables de texto
 
 # Comparadores
-# Ingrese dos palabras cualesquiera y realice las sigueintes
+# Ingrese dos palabras cualesquiera y realice las siguientes
 # comparaciones entre ellas
 texto_1 = str(input('Ingrese la primera palabra:\n'))
 
@@ -19,18 +19,48 @@ texto_2 = str(input('Ingrese la segunda palabra:\n'))
 # Compare cual de las dos palabras es mayor (alfabéticamente)
 # Imprima en pantalla según corresponda
 
+if texto_1 > texto_2:
+    print(f'{texto_1} es mayor a {texto_2}')
+
+else:
+    print(f'{texto_2} es mayor a {texto_1}')
+
 # Compare cual de las dos palabras tiene mayor
 # cantidad de letras
 # Imprima en pantalla según corresponda
 
+if len(texto_1) > len(texto_2):
+    print(f'{texto_1} tiene mas letras que {texto_2}')
+
+else:
+    print(f'{texto_2} tiene mas letras que {texto_1}')
+
 # Verifique si la primera letra de la primera palabra
 # es mayor a la primera letra de la segunda palabra
 # Imprima en pantalla según corresponda
+
+if texto_1[0] > texto_2[0]:
+    print(f'{texto_1[0]} es mayor a {texto_2[0]}')
+
+else:
+    print(f'{texto_2[0]} es mayor a {texto_1[0]}')
 
 copia_texto_1 = texto_1  # Copia de la variable texto_1
 
 # Verifique que copia_texto_1 es igual a texto_1
 # Imprima en pantalla según corresponda
 
+if copia_texto_1 == texto_1:
+    print(f'{copia_texto_1} es igual a {texto_1}')
+
+else:
+    print(f'{copia_texto_1} es diferente a {texto_1}')
+
 # Verifique que copia_texto_1 es distinta a texto_2
 # Imprima en pantalla según corresponda
+
+if copia_texto_1 != texto_2:
+    print(f'{copia_texto_1} es diferente a {texto_2}. Terminamos!')
+
+else:
+    print('Terminamos!')
